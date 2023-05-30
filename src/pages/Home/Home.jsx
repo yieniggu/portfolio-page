c7e20a5500cb4b77f00a2227f08aa737a91d5ca1
Bastiloko <bastianandresxb@gmail.com>
2023-05-30 18:11:12 -0400

    assets: modified home icons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { Layout } from "../../layout";
 import GreenCodeMatrix from "../../assets/videos/green-code-matrix2.mp4";
 import Cameras from "../../assets/images/cameras.jpg";
-import OpenFolder from "../../assets/images/open-folder.png";
-import ClosedFolder from "../../assets/images/closed-folder.png";
-import File from "../../assets/images/file-icon.png";
+import OpenFolder from "../../assets/images/open-folder2.png";
+import ClosedFolder from "../../assets/images/closed-folder2.png";
+import Disk from "../../assets/images/diskette2.png";
 
 export const Home = () => {
   const [proyectsToggled, setProyectsToggled] = useState(false);
@@ -35,46 +35,56 @@ export const Home = () => {
                   </span>
                 </h1>
                 <h2 className="text-center text-matrix-dark-green text-2xl mt-20 font-code-75 tracking-tight">
-                  {">"}DESCONOCIDO$ QUE DECISION TOMARAS
+                  {">"}DESCONOCIDO$ QUE DECISION TOMARAS?
                   <span className="animate-ping transition duration-75">|</span>
                 </h2>
-                <div className="flex flex-row justify-between mt-10">
+                <div className="flex flex-row justify-around mt-10">
                   <div
-                    className="flex flex-col group cursor-pointer hover:-translate-y-4 transition duration-300"
+                    className="flex flex-col group cursor-pointer hover:-translate-y-4 transition duration-300 mx-auto my-auto"
                     onClick={() => setProyectsToggled(!proyectsToggled)}
                   >
                     <div className="relative">
                       <img
-                        className="w-1/2 mx-auto "
+                        className="shadow-md shadow-matrix-dark-green rounded-md"
                         src={proyectsToggled ? OpenFolder : ClosedFolder}
+                        width={proyectsToggled ? 130 : 120}
                       />
 
-                      <h3 className="absolute top-1/2 right-[100px] text-xl font-cyber text-gray-300 -rotate-6">
-                        Proyectos
+                      <h3
+                        className={`absolute top-1/2 w-full text-center text-xl font-cyber text-gray-300 -rotate-6 bg-black ${
+                          proyectsToggled && "hidden"
+                        }`}
+                      >
+                        Projects
                       </h3>
                     </div>
                   </div>
-                  <div className="relative cursor-pointer hover:-translate-y-4 transition duration-300">
+                  <div className="relative cursor-pointer hover:-translate-y-4 transition duration-300 mx-auto">
                     <img
-                      className="w-1/2 my-auto mx-auto shadow-xl"
-                      src={File}
+                      className="my-auto mx-auto shadow-md rounded-md shadow-matrix-dark-green"
+                      src={Disk}
+                      width={140}
                     />
 
-                    <h3 className="absolute top-1/4 right-1/3 text-md text-center font-cyber text-red-500">
-                      SECRET
+                    <h3 className="absolute top-10 text-md text-center w-full border-2 border-red-700 font-cyber text-red-700 bg-red-500">
+                      CONFIDENTIAL
                     </h3>
-                    <h3 className="absolute top-1/2 right-[110px] text-xl font-cyber text-gray-300 -rotate-6">
-                      Sujeto1
+                    <h3 className="absolute top-20 w-full text-center text-xl font-cyber text-gray-300 -rotate-6 bg-black px-4">
+                      SubjectX
                     </h3>
                   </div>
-                  <div className="relative cursor-pointer hover:-translate-y-4 transition duration-300">
-                    <img className="w-1/2 my-auto mx-auto" src={File} />
+                  <div className="relative cursor-pointer hover:-translate-y-4 transition duration-300 mx-auto">
+                    <img
+                      className="my-auto mx-auto shadow-md rounded-md shadow-matrix-dark-green"
+                      src={Disk}
+                      width={140}
+                    />
 
-                    <h3 className="absolute top-1/4 right-1/3 text-md font-cyber text-red-500">
-                      SECRET
+                    <h3 className="absolute top-10 text-md text-center w-full font-cyber border-2 border-red-700 text-red-700 bg-red-500">
+                      CONFIDENTIAL
                     </h3>
-                    <h3 className="absolute top-1/2 right-[110px] text-xl font-cyber text-gray-300 -rotate-6">
-                      Sujeto2
+                    <h3 className="absolute top-20 w-full text-center text-xl font-cyber text-gray-300 -rotate-6 bg-black px-4">
+                      SubjectY
                     </h3>
                   </div>
                 </div>
