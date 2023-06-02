cceedcac06d0931a367cbc2afb45f35168ed7d9c
Bastiloko <bastianandresxb@gmail.com>
2023-06-01 21:26:07 -0400

    added screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,15 @@ export default {
         },
       },
     },
+    screens: {
+      tiny: "360px",
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1336px",
+      "3xl": "1920px",
+    },
   },
   plugins: [],
 };
